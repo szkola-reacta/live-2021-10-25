38f90a9c8b740ca1dfd9075a9deabaae20213695
Patryk Omiotek <patryk.omiotek@gmail.com>
2021-10-25 20:13:48 +0200

    refs for email

--- src/Hello/Hello.js
+++ src/Hello/Hello.js
@@ -1,12 +1,27 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 function Hello({ condition }) {
   // const [name, setName] = useState(() => {
   //   return window.localStorage.getItem("name") || ""
   // });
   const [name, setName] = useState(window.localStorage.getItem("name") || "");
+  const emailFieldRef = useRef(null);
   const handleNameChange = (event) => {
     setName(event.target.value)
+  }
+  const hanleEmailChange = () => {
+    const elem = emailFieldRef.current;
+    if (elem) {
+      const isValid = elem.value.includes('@');
+      if (!isValid) {
+        // elem.value
+        elem.style.borderColor = '#f00';
+        elem.style.color = '#f00';
+      } else {
+        elem.style.borderColor = '#000';
+        elem.style.color = '#000';
+      }
+    }
   }
 
   if (condition === 2) {
@@ -27,7 +42,12 @@ function Hello({ condition }) {
       </div>
       <div>
         <label htmlFor="email321">E-mail</label>
-        <input id="email321" type="email" />
+        <input
+          ref={emailFieldRef}
+          id="email321"
+          type="email"
+          onChange={hanleEmailChange}
+        />
       </div>
     </div>
   );
